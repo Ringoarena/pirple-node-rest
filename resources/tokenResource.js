aa69723c398b73ee2d48eac5f9cc4342aa3df4ef
Rikard Virta <rikard.virta90@gmail.com>
2021-12-23 19:07:11 +0100

    token service is now looking up the userData from the userService

--- resources/tokenResource.js
+++ resources/tokenResource.js
@@ -1,30 +1,17 @@
 const tokenService = require('../services/tokenService')
-const userService = require('../services/userService')
 
 const handlers = {
   post: (data, callback) => {
     var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false
     var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length ? data.payload.password.trim() : false
     if (phone && password) {
-      userService.getUserByPhone(phone, (error, userData) => {
-        if (!error && userData) {
-          var encryptedPassword = userService.encrypt(password)
-          if (encryptedPassword == userData.encryptedPassword) {
-            tokenService.authenticate(phone, (error, tokenData) => {
-              if (!error && tokenData) {
-                callback(201, { token: tokenData })
-              } else {
-                callback(500, { error: 'could not authenticate' })
-              }
-            })
-          } else {
-            callback(400, { error: 'incorrect password'})
-          }
+      tokenService.authenticate(phone, password, (error, tokenData) => {
+        if (!error && tokenData) {
+          callback(201, { token: tokenData })
         } else {
-          callback(404, { error: 'user not found'})
+          callback(500, error)
         }
       })
-
     } else {
       callback(400, { error: 'missing required fields'})
     }
--- services/tokenService.js
+++ services/tokenService.js
@@ -1,22 +1,33 @@
-var tokenRepository = require ('../repositories/tokenRepository')
+var tokenRepository = require('../repositories/tokenRepository')
+var userService = require('./userService')
 
 var uuid = () => {
   return Math.random().toString(16).slice(2)
 }
 
+
+
 var tokenService = {
-  authenticate: (phone, callback) => {
-    var tokenData = {
-      id: uuid(),
-      expires: Date.now() * 1000 * 60 * 60,
-      phone
-    }
-    tokenRepository.create(tokenData, (error) => {
-      if (error) {
-        console.log(error)
-        callback(error)
+  authenticate: (phone, password, callback) => {
+    userService.getUserByPhone(phone, (error, userData) => {
+      if (!error && userData) {
+        var encryptedPassword = userService.encrypt(password)
+        if (encryptedPassword == userData.encryptedPassword) {
+          var tokenData = {
+            id: uuid(),
+            expires: Date.now() * 1000 * 60 * 60,
+            phone
+          }
+          tokenRepository.create(tokenData, (error) => {
+            if (!error) {
+              callback(false, tokenData)
+            } else {
+              callback(error)
+            }
+          })
+        }
       } else {
-        callback(false, tokenData)
+        callback(error)
       }
     })
   },
