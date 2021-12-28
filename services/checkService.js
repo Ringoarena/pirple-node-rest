ffe119af7d46b029135730ecaa91d9939625e825
Rikard Virta <rikard.virta90@gmail.com>
2021-12-28 16:20:10 +0100

    fixed bug, reason was cyclic dependencies

--- services/checkService.js
+++ services/checkService.js
@@ -1,11 +1,11 @@
-var config = require('../config')
 var checkRepository = require('../repositories/checkRepository')
-var userService = require('./userService')
+var userRepository = require('../repositories/userRepository')
+var config = require('../config')
 var uuidGenerator = require('../lib/uuidGenerator')
 
 var checkService = {
   createCheck: (phone, checkData, callback) => {
-    userService.getUserByPhone(phone, (error, userData) => {
+    userRepository.read(phone, (error, userData) => {
       if (!error && userData) {
         var userChecks = typeof(userData.checks) == 'object' && userData.checks instanceof Array ? userData.checks : []
         if (userChecks.length < config.maxChecks) {
@@ -18,7 +18,7 @@ var checkService = {
             if (!error) {
               userData.checks = userChecks
               userData.checks.push(newCheck.id)
-              userService.updateUser(userData, (error) => {
+              userRepository.update(userData, (error) => {
                 if (!error) {
                   callback(false, newCheck)
                 } else {
@@ -42,6 +42,28 @@ var checkService = {
   },
   updateCheck: (checkData, callback) => {
     checkRepository.update(checkData, callback)
+  },
+  deleteCheck: (checkData, callback) => {
+    checkRepository.delete(checkData.id, (error) => {
+      if (!error) {
+        userRepository.read(checkData.userPhone, (error, userData) => {
+          if (!error && userData) {
+            var userChecks = typeof(userData.checks) == 'object' && userData.checks instanceof Array ? userData.checks : []
+            var checkIndex = userChecks.indexOf(checkData.id)
+            if (-1 < checkIndex) {
+              userChecks.splice(checkIndex, 1)
+              userRepository.update(userData, callback)
+            } else {
+              callback({ error: 'checkId not found on user' })
+            }
+          } else {
+            callback(error)
+          }
+        })
+      } else {
+        callback(error)
+      }
+    })
   }
 }
 
--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,7 @@
-var crypto = require('crypto');
 var userRepository = require('../repositories/userRepository')
+var checkRepository = require('../repositories/checkRepository')
 var config = require('../config')
+var crypto = require('crypto');
 
 var userService = {
   createUser: (userData, callback) => {
@@ -13,7 +14,36 @@ var userService = {
     userRepository.update(userData, callback)
   },
   deleteUser: (userData, callback) => {
-    userRepository.delete(userData, callback)
+    userRepository.delete(userData, (error) => {
+      if (!error) {
+        var userCheckIds = typeof(userData.checks) == 'object' && userData.checks instanceof Array ? userData.checks : []
+        var nChecksToDelete = userCheckIds.length
+        if (0 < nChecksToDelete) {
+          var nChecksDeleted = 0
+          var deletionErrors = []
+          userCheckIds.forEach((checkId) => {
+            checkRepository.delete(checkId, (error) => {
+              if (error) {
+                deletionErrors.push(error)
+              }
+              nChecksDeleted++;
+              if (nChecksDeleted == nChecksToDelete) {
+                if (!deletionErrors.length) {
+                  callback(false)
+                } else {
+                  callback({ deletionErrors })
+                }
+              }
+
+            })
+          })
+        } else {
+          callback(false)
+        }
+      } else {
+        callback(error)
+      }
+    })
   },
   encrypt: (str) => {
     if (typeof(str) == 'string' && str.length) {
