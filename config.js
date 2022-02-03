d3c7130c8532145e80fe7a730f1c82f504dc8c46
Rikard Virta <rikard.virta90@gmail.com>
2022-02-03 22:39:29 +0100

    twilioServiceClient finished. We need to get a real token though.

--- config.js
+++ config.js
@@ -4,14 +4,24 @@ var environments = {
     httpPort: 3000,
     httpsPort: 3001,
     privateKey: 'thisIsASecret',
-    maxChecks: 5
+    maxChecks: 5,
+    twilio: {
+      accountId: 'ACb32d411ad7fe886aac54c665d25e5c5d',
+      authToken: '9455e3eb3109edc12e3d8c92768f7a67',
+      fromPhone: '+15005550006',
+    }
   },
   production: {
     name: 'production',
     httpPort: 5000,
     httpsPort: 5001,
     privateKey: 'thisIsAlsoASecret',
-    maxChecks: 5
+    maxChecks: 5,
+    twilio: {
+      accountId: '',
+      fromPhone: '',
+      authToken: ''
+    }
   }
 };
 
--- serviceClients/twilioClient.js
+++ serviceClients/twilioClient.js
@@ -1,37 +1,47 @@
 var querystring = require('querystring')
 var https = require('https')
-
-var cfg = {
-  fromPhone: '',
-  accountId: 'asd',
-  authToken: ''
-}
+var config = require('../config')
 
 var twilioClient = {
   sendTwilioSMS: (phone, message, callback) => {
-    phone = typeof(phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false
+    phone = typeof(phone) == 'string' && phone.trim().length == 13 ? phone.trim() : false
     message = typeof(message) == 'string' && message.trim().length > 0 && message.trim().length <= 1600 ? message.trim() : false
     var inputIsValid = phone && message
     if (inputIsValid) {
       var payload = {
-        from: cfg.fromPhone,
+        from: config.fromPhone,
         to: phone,
         body: message
       }
       var stringPayload = querystring.stringify(payload)
       var requestDetails = {
-        protocol: 'https',
+        protocol: 'https:',
         hostname: 'api.twilio.com',
         method: 'POST',
-        path: `/2010-04-01/Accounts/${cfg.accountId}/Messages.json`,
-        auth: `${cfg.accountId}/${cfg.authToken}`,
+        path: `/2010-04-01/Accounts/${config.twilio.accountId}/Messages.json`,
+        auth: `${config.twilio.accountId}/${config.twilio.authToken}`,
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'Content-Length': Buffer.byteLength(stringPayload),
         }
       }
+      var request = https.request(requestDetails, (response) => {
+        var status = response.statusCode
+        if (status == 200 || status == 201) {
+          callback(false)
+        } else {
+          callback({ error: `twilio reponse.status was ${status}` })
+        }
+      })
+      request.on('error', (error) => {
+        callback(error)
+      })
+      request.write(stringPayload)
+      request.end()
     } else {
       callback({ error: 'invalid input' })
     }
   }
 }
+
+module.exports = twilioClient
